b9956d1a1f6156e9179abf57aaa92ced89ce9fb0
sadiquex <abubakasaddik1@gmail.com>
2023-09-26 14:51:11 +0000

    changed the homepage

--- app/page.js
+++ app/page.js
@@ -2,5 +2,10 @@ import { createElement as $ } from "react";
 import { H1 } from "./_ui/styles/dsystem/textSystem";
 
 export default function Home() {
-  return $("main", null, $(H1, null, `This is the '/' page`));
+  return $(
+    "main",
+    null,
+    $(H1, null, `This is the '/' page`),
+    $("p", null, "homepage changes")
+  );
 }
